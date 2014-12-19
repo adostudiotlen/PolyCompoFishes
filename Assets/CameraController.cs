e3a5754e38a457d039162a28b401e303acff278f
Przemotar <przemotar@gmail.com>
2014-12-20 00:03:24 +0100

    rybe leci i skreca ogonem

---
+++ Assets/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+
+	GameObject player;
+
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void Update()
+	{
+		this.transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, this.transform.position.z); 
+	}
+
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	float angle;
+	GameObject tail;
+
+	void Awake()
+	{
+		tail = transform.FindChild ("Tail").gameObject;
+	}
+
+	void Update()
+	{
+
+	}
+
+	void FixedUpdate()
+	{
+		if(Input.GetKey (KeyCode.W))
+		{
+
+			this.rigidbody2D.AddForce(-this.transform.right * 0.1f, ForceMode2D.Impulse );
+		}
+
+		angle = Input.GetAxis ("Horizontal");
+		Debug.Log (angle);
+		//this.rigidbody2D.MoveRotation (angle * 45.0f);
+		transform.Rotate (-this.transform.forward, angle * 45.0f * 0.05f);
+		tail.transform.localRotation = Quaternion.AngleAxis (90 + angle * 45.0f, this.transform.forward);
+	}
+}
